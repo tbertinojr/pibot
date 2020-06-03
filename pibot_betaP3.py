71e96b4d62494cc916ba8d423186b34b7b5d6f7a
tbertinojr <tbertinojr@gmail.com>
2020-06-02 21:55:40 -0500

    Update pibot_betaP3.py

--- pibot_betaP3.py
+++ pibot_betaP3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 import time
 from time import sleep
+from typing import Tuple
+
 import RPi.GPIO as GPIO
 import curses
 import sys, tty, termios
@@ -22,14 +24,10 @@ Motor_A_1 = 3
 Motor_A_2 = 5
 Motor_B_1 = 11
 Motor_B_2 = 13
-# Dir_forward   = 0
-# Dir_backward  = 1
-# global pwm_a
-# global pwm_b
+
 
 allGPIO_list = (3, 5, 7, 11, 13, 15)
 
-<<<<<<< HEAD
 Motor_A_EN = 7  # GPIO BORAD PIN 7
 Motor_B_EN = 15  # GPIO BOARD PIN 15
 Motor_A_1 = 3
@@ -44,15 +42,13 @@ pwm_b = None
 allGPIO_list = (3, 5, 7, 11, 13, 15)
 
 
-def setup():  # Motor initialization
-=======
 
 
 def Motor_A_FWD():
     GPIO.output(11, True)
     GPIO.output(13, False)
 
-def Motor_B_FWD(): 
+def Motor_B_FWD():
     GPIO.output(3, False)
     GPIO.output(5, True)
 
@@ -66,7 +62,6 @@ def Motor_B_REV():
 
 def setup():  # Motor initialization
     global pwm_a, pwm_b
->>>>>>> 43d91ba4bb741748abace786ad27114a7722a92c
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(Motor_A_EN, GPIO.OUT)
@@ -88,22 +83,8 @@ def forward(x):  # Forward Continuous
     Motor_B_FWD()
     speed()
     sleep(x)  # ADD ENCODER FOR SYNC FEEDBACK
-<<<<<<< HEAD
 
 
-=======
-
-def test():
-    Motor_B_FWD()
-    pwm_b.ChangeDutyCycle(80)
-    sleep(1.5)
-    stopAll()
-    Motor_B_REV()
-    pwm_b.ChangeDutyCycle(80)
-    sleep(1.5)
-    stopAll()
-
->>>>>>> 43d91ba4bb741748abace786ad27114a7722a92c
 def reverse(x):  # Reverse Continuous
     Motor_A_REV()
     Motor_B_REV()
@@ -113,7 +94,7 @@ def reverse(x):  # Reverse Continuous
 
 def left():  # Short Left Turn, Then Sleep
     Motor_A_REV()
-    Motor_B_FWD()    
+    Motor_B_FWD()
     speed()
     sleep(0.175)
     GPIO.output(allGPIO_list, False)
@@ -127,31 +108,24 @@ def right():  # Short Right Turn, Then Sleep
     stopAll()
 
 
-def left_ninety():
-    GPIO.output(5, True)
-    GPIO.output(3, False)
-    GPIO.output(13, True)
-    GPIO.output(11, False)
+def left_ninety():  #Turn Left 90 degrees
+    Motor_A_REV()
+    Motor_B_FWD()
     speed()
     sleep(0.85)
     stopAll()
 
 
-def nineT_right():
-    GPIO.output(3, True)
-    GPIO.output(5, False)
-    GPIO.output(11, True)
-    GPIO.output(13, False)
+def nineT_right():  #turn right 90 degrees
+    Motor_A_FWD()
+    Motor_B_REV()
     speed()
     sleep(0.85)
     stopAll()
 
 
 def stopAll():
-   # pwm_a.ChangeDutyCycle(0)
-   # pwm_b.ChangeDutyCycle(0) Test to remove
     GPIO.output(allGPIO_list, False)
-
 
 def speed():
     pwm_a.ChangeDutyCycle(100)
@@ -159,11 +133,7 @@ def speed():
 
 
 setup()
-<<<<<<< HEAD
-Gamepad.init()
-=======
 #Gamepad.__init__(XboxOne)
->>>>>>> 43d91ba4bb741748abace786ad27114a7722a92c
 # PWM Enabled @ 0%
 pwm_a.start(0)
 pwm_b.start(0)
@@ -171,8 +141,6 @@ GPIO.output(Motor_A_EN, True)
 GPIO.output(Motor_B_EN, True)
 
 getch = getGetch._Getch()
-<<<<<<< HEAD
-
 
 ## FOR FUTURE XBOX ONE CONTROLLER CONNECTIVITY
 def connect():  # asyncronus read-out of events
@@ -188,7 +156,6 @@ def connect():  # asyncronus read-out of events
             return remote_control
     return None
 
-
 def is_connected():  # asyncronus read-out of events
     path = None
     devices = [InputDevice(path) for path in list_devices()]
@@ -200,11 +167,6 @@ def is_connected():  # asyncronus read-out of events
         return False
     return True
 
-
-connect()
-
-=======
->>>>>>> 43d91ba4bb741748abace786ad27114a7722a92c
 screen = curses.initscr()
 curses.noecho()
 curses.cbreak()
@@ -242,13 +204,7 @@ try:
             test()
 finally:
     # Close down curses properly,  turn echo back on!
-<<<<<<< HEAD
-    curses.nocbreak()
-    screen.keypad(0)
-    curses.echo()
-=======
     curses.nocbreak();screen.keypad(0);curses.echo()
->>>>>>> 43d91ba4bb741748abace786ad27114a7722a92c
     curses.endwin()
     GPIO.cleanup()
     # END OF PROGRAM
