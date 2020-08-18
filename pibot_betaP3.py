7955b3ce69c15d6d4d56a1e7ccdb75a352e24c21
tbertinojr <tbertinojr@gmail.com>
2020-08-18 10:01:00 -0500

    Update pibot_betaP3.py

--- pibot_betaP3.py
+++ pibot_betaP3.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
-import time
-from time import sleep
-from typing import Tuple
+#import time
+#from time import sleep
+#from typing import Tuple
 import RPi.GPIO as GPIO
 import curses
 import sys, tty, termios
 sys.path.append( "/usr/lib/python3/dist-packages" )
 sys.path.append( "/home/pi/pibot/pibot" )
-import Gamepad as GP
-import os, struct, array
-from fcntl import ioctl
+#import Gamepad as GP
+#import os, struct, array
+#from fcntl import ioctl
 from evdev import ecodes, InputDevice, ff, util, list_devices
 import asyncio
 import getGetch
@@ -93,7 +93,7 @@ loop = asyncio.get_event_loop()
 signals = (signal.SIGHUP, signal.SIGTERM, signal.SIGINT)
 
 for s in signals:
-    loop.add_signal_handler(s, lambda s=s: asyncio.create_task(shutdown_signal(s, loop)))
+    loop.add_signal_handler(s,lambda s=s: asyncio.create_task(shutdown_signal(s, loop)))
     try:
         remote_control = connect_gamepad()
         if remote_control is None:
@@ -101,13 +101,15 @@ for s in signals:
             sys.exit()
 
         remote_control.rumble_effect = 2
-        tasks = [remote_control.read_gamepad_input(), remote_control.rumble(), read_gamepad_inputs()]
-        loop.run_until_complete(asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED))
+        tasks = [remote_control.read_gamepad_input(),
+                 remote_control.rumble(), read_gamepad_inputs()]
+        loop.run_until_complete(asyncio.wait(tasks,
+                                             return_when=asyncio.FIRST_COMPLETED))
         loop.run_until_complete(removetasks(loop))
     except:
+        GPIO.cleanup()
         print('Exiting Program')
         sys.exit()
-        GPIO.cleanup()
     finally:
         GPIO.cleanup()
 #############################
@@ -117,36 +119,36 @@ screen.keypad( True )
 
 # DRIVE COMMANDS INPUT FROM KEYBOARD
 try:
-    while True:
-        char = screen.getch()
-        if char == ord( 'q' ):
-            break
-        elif char == curses.KEY_UP:
-            pbot.forward()
-        elif char == curses.KEY_DOWN:
-            pbot.reverse()
-        elif char == curses.KEY_RIGHT:
-            pbot.right()
-        elif char == curses.KEY_LEFT:
-            pbot.left()
-        elif char == ord( 'w' ):
-            forward( 0.1 )
-        elif char == ord( 's' ):
-            reverse( 0.1 )
-        elif char == ord( 'd' ):
-            right()
-        elif char == ord( 'a' ):
-            left()
-        elif char == ord( 'l' ):
-            stopAll()
-        elif char == ord( '1' ):
-            left_ninety()
-        elif char == ord( '2' ):
-            nineT_right()
+    # while True:
+    #     char = screen.getch()
+    #     if char == ord( 'q' ):
+    #         break
+    #     elif char == curses.KEY_UP:
+    #         pbot.forward()
+    #     elif char == curses.KEY_DOWN:
+    #         pbot.reverse()
+    #     elif char == curses.KEY_RIGHT:
+    #         pbot.right()
+    #     elif char == curses.KEY_LEFT:
+    #         pbot.left()
+    #     elif char == ord( 'w' ):
+    #         forward( 0.1 )
+    #     elif char == ord( 's' ):
+    #         reverse( 0.1 )
+    #     elif char == ord( 'd' ):
+    #         right()
+    #     elif char == ord( 'a' ):
+    #         left()
+    #     elif char == ord( 'l' ):
+    #         stopAll()
+    #     elif char == ord( '1' ):
+    #         left_ninety()
+    #     elif char == ord( '2' ):
+    #         nineT_right()
     
 finally:
     # Close down curses properly,  turn echo back on!
-    screen.keypad( 0 );
+    screen.keypad(False);
     curses.endwin()
     GPIO.cleanup()
     # END OF PROGRAM
